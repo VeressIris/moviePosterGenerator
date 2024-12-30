94c290282d4361625d16a28e7e94f8390232acef
VeressIris <irisveress@gmail.com>
2024-12-30 02:26:12 +0200

    added poster layout

---
+++ src/lib/PosterLayout.svelte
@@ -0,0 +1,27 @@
+<script>
+	let { imgSrc, title, releaseDate, runtime, genres, director, actors, colorPalette } = $props();
+</script>
+
+<div class="flex flex-col items-start">
+	<div class="mb-2 flex w-full justify-between">
+		<div class="flex">
+			{#each colorPalette as color}
+				<div class="h-8 w-8" style="background-color: {color}"></div>
+			{/each}
+		</div>
+		<img src="../that globe in a rectangle every design uses.png" alt="globe" class="h-8" />
+	</div>
+	<img src={imgSrc} alt="poster" class="mb-4 h-[800px] w-[600px] object-cover" />
+	<div class="flex flex-col">
+		<div class="mb-2 flex items-end">
+			<h1 class="mr-2">{title}</h1>
+			<p class="text-xl">{releaseDate}</p>
+		</div>
+		<p>Genres: <span class="font-bold">{genres.map((genre) => ` ${genre.toUpperCase()}`)}</span></p>
+		<p>Runtime: <span class="font-bold">{runtime.toUpperCase()}</span></p>
+		<p>Directed by: <span class="font-bold">{director.toUpperCase()}</span></p>
+		<p class="mt-2">
+			Starring: <span class="font-bold">{actors.map((actor) => ` ${actor.toUpperCase()}`)}</span>
+		</p>
+	</div>
+</div>
---
+++ src/routes/complete/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import PosterLayout from '$lib/PosterLayout.svelte';
+</script>
+
+<div class="mx-8 my-4 flex flex-col items-center">
+	<PosterLayout
+		imgSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercosmos.com%2Fw%2Ffull%2F7%2F2%2F4%2F172506-2984x2000-desktop-hd-the-shawshank-redemption-background-image.jpg&f=1&nofb=1&ipt=23696381a15f291c9257783d9703a13fff365db20e528a93112744605c4c1d8a&ipo=images"
+		title="THE SHAWSHANK REDEMPTION"
+		releaseDate="1994"
+		runtime="142 min"
+		genres={['Drama', 'Historical']}
+		director="Frank Darabont"
+		actors={['Tim Robbins', 'Morgan Freeman']}
+		colorPalette={['rgb(0,0,0)', '#FFFFFF', '#C0C0C0', '#808080', '#FF0000']}
+	/>
+</div>
