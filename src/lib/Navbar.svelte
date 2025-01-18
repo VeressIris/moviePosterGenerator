ce8f710d2a27378ec714e6682d6dc300b556bfcb
VeressIris <irisveress@gmail.com>
2025-01-18 19:54:44 +0200

    added more to home page
    made more responsive

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,7 +1,15 @@
-<div class="bg-off-white-200 flex w-full items-center justify-between px-8 py-4">
-	<h3 class="max-w-48 text-xl sm:w-fit">Minimalist Poster Generator</h3>
+<div class="bg-off-white-200 flex w-full items-center justify-between px-8 py-4 2xl:px-24">
+	<h3 class="max-w-48 text-xl sm:w-fit lg:max-w-fit">Minimalist Poster Generator</h3>
 	<div>
-		<a href="/" class="mr-2">About</a>
-		<a href="/generate">Generate</a>
+		<a
+			href="/"
+			class="border-dove-gray-400 hover:text-dove-gray-400 mr-2 transition-all duration-[50ms] ease-in-out hover:scale-0 hover:cursor-pointer hover:border-b-2"
+			>About</a
+		>
+		<a
+			href="/generate"
+			class="border-dove-gray-400 hover:text-dove-gray-400 transition-all duration-[50ms] ease-in-out hover:scale-0 hover:cursor-pointer hover:border-b-2"
+			>Generate</a
+		>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,38 +2,47 @@
 	<title>Minimalist Posters | Home</title>
 </svelte:head>
 
-<div class="mx-8 my-4 pb-8">
-	<h1 class="mb-2">The simplest way to generate a minimalist poster.</h1>
-	<p class="text-xl">
+<div class="m-8 pb-8 2xl:mx-24">
+	<h1 class="mb-2 md:text-5xl lg:text-6xl">The simplest way to generate a minimalist poster.</h1>
+	<p class="mb-4 text-xl md:text-2xl lg:text-3xl">
 		Create a minimalist poster for your favorite <span class="font-semibold italic"
 			>movie or TV show</span
 		> in seconds.
 	</p>
-	<div class="flex w-full justify-center">
+	<div class="flex w-full items-end">
+		<a
+			href="/generate"
+			class="text-off-white-100 active:bg-cyan-1100 hover:bg-cyan-1000 rounded-xl bg-cyan-900 px-5 py-2 text-center text-xl font-bold hover:cursor-pointer md:px-6 md:text-2xl"
+			>Try it</a
+		>
+		<p class="ml-4 text-xl md:ml-3">It's <i>free</i>. And <i>fast</i>.</p>
+	</div>
+	<h1 class="mt-4 underline lg:mt-8">Some results:</h1>
+	<div class="lg:bg-off-white-200 my-4 flex w-full justify-center lg:px-8 lg:py-4 2xl:px-24">
 		<div
-			class="mt-2 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:flex 2xl:w-full 2xl:justify-between"
+			class="2xl:flex-warp grid gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:flex 2xl:w-full 2xl:flex-wrap 2xl:justify-between"
 		>
 			<img
-				class="border-dove-gray-100 mt-4 w-[450px] rounded-lg border-2"
+				class="border-dove-gray-100 w-[450px] rounded-lg border-2 2xl:w-[525px]"
 				src="minimal Interstellar poster.jpg"
 				alt="minimalist interstellar poster"
 			/>
 			<img
-				class="border-dove-gray-100 mt-4 w-[450px] rounded-lg border-2"
+				class="border-dove-gray-100 w-[450px] rounded-lg border-2 2xl:w-[525px]"
 				src="minimal Fight Club poster.jpg"
 				alt="minimalist fight club poster"
 			/>
 			<img
-				class="border-dove-gray-100 mt-4 w-[450px] rounded-lg border-2"
+				class="border-dove-gray-100 w-[450px] rounded-lg border-2 2xl:w-[525px]"
 				src="minimal Squid Game poster.jpg"
 				alt="minimalist squid game season 1 poster"
 			/>
 		</div>
 	</div>
-	<h1 class="mb-2 mt-6">No need to do anything.</h1>
+	<h1 class="mb-1 mt-6">No need to do anything.</h1>
 	<p class="text-xl">Just upload your image & we'll do the rest.</p>
 	<div id="about">
-		<h1 class="mb-2 mt-6">What do we offer?</h1>
+		<h1 class="mb-2 mt-6 underline">What do we offer?</h1>
 		<ul class="ml-8 list-disc">
 			<li class="text-xl">
 				A fast and <span class="font-semibold italic">effortless</span> way to create
